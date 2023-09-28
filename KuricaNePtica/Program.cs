05a8fd9ec62d231b1ad1083fa8b33a3ae9d98e5c
mushroomvoodoo <normeeboy@gmail.com>
2023-09-28 08:34:56 +0300

    конец. победа.

--- KuricaNePtica/Program.cs
+++ KuricaNePtica/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿internal class Program
+{
+    static void Chicken()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("Курица");
+        }
+    }
+    private static void Main(string[] args)
+    {
+
+        Thread chick = new(Chicken);
+        chick.Priority = ThreadPriority.Highest;
+        chick.Start();
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("Яйцо");
+        }
+        Winner(chick);
+        static void Winner (Thread chicken)
+        {
+            Console.WriteLine(chicken.IsAlive ? "Конец. Победа курицы" : "Конец. Победа яйца");
+        }
+    }
+}
